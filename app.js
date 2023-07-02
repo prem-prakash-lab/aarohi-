7141bfb61ecfaad99d7d6273b59c890c932aa892
prem-prakash-lab <prakashmadhepur@gmail.com>
2023-07-02 09:56:34 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+
+/**
+ * @author prem prakash <prakashmadhepur@gmail.com>
+ */
+(function ($) {
+    "use strict";
+      $('.sakura-falling').sakura();
+})(jQuery);
+
+$(document).on('click', function(){
+    document.getElementById("my_audio").play();
+    console.log('Shaadi me zaroor aana');
+});
+
+const images = document.querySelectorAll('.animate-image');
+let index = 0;
+
+function showImage() {
+  images[index].classList.add('show');
+  index++;
+  if (index < images.length) {
+    setTimeout(showImage, 3000); // Adjust the delay between each image (in milliseconds)
+  }
+}
+
+showImage();
+
+
